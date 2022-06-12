70786a592129822ed00e0c8ef99cfbbf6a639fb5
maks6169 <maks6169@rambler.ru>
2022-06-12 21:57:41 +0300

    1

--- src/ru/geekbrains/java2/Main.java
+++ src/ru/geekbrains/java2/Main.java
@@ -32,13 +32,13 @@ public class Main {
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
         Thread t1 = new Thread(() -> {
-            for (int i = 0; i < h; i++) {
-                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            for (int i = 0; i < a1.length; i++) {
+                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
         Thread t2 = new Thread(() -> {
-            for (int i = 0; i < h; i++) {
-                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            for (int i = 0; i < a2.length; i++) {
+                a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
         t1.start();
