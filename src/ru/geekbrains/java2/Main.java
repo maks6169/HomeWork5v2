1df6a75f2fb1ccb22b098014f0a00bce44ebabdb
maks6169 <maks6169@rambler.ru>
2022-06-12 21:53:25 +0300

    Initial commit

---
+++ src/ru/geekbrains/java2/Main.java
@@ -0,0 +1,57 @@
+package ru.geekbrains.java2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        arrDoubleThread(arr);
+        arrOneThread(arr);
+    }
+
+    private static final int size = 10000000;
+    private static final int h = size / 2;
+    private static float[] arr = new float[size];
+
+
+    public static void arrOneThread(float[] arr) {
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.currentTimeMillis();
+        System.out.println(System.currentTimeMillis() - time);
+    }
+
+    public static void arrDoubleThread(float[] arr) {
+        long time = System.currentTimeMillis();
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        System.out.println(System.currentTimeMillis() - time);
+    }
+}
